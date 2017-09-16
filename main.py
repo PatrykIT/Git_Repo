31d3c6c281caad5c7cfbe7b2010ab45c48cc10be
Zyzz <patry14inf@gmail.com>
2017-09-16 13:04:23 +0200

    Removed redundant parameters passed to methods.

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ from string import ascii_lowercase
 
 
 class Siatka:
-    'Klasa zarzadza gra. Jest to siatka ktora przetrzymuje informacje co znajduje sie na planszy'
+    """Jest to siatka ktora przetrzymuje informacje co znajduje sie na planszy."""
 
-    rozmiar_siatki = 9
+    rozmiar_siatki = 5
     miny = []
-    numer_min = 10
+    numer_min = 2
     plansza = []
+    plansza_widoczna_dla_gracza = []
     flagi = []
 
     def get_rozmiar_siatki(self):
@@ -22,22 +23,23 @@ class Siatka:
 
     # TODO: def __init__ for assignment of all variables
 
-    def stworz_plansze(self, start):
+    def stworz_plansze(self, poczatkowa_komorka):
         pusta_siatka = [['0' for i in range(self.rozmiar_siatki)] for i in range(self.rozmiar_siatki)]
 
-        self.ustaw_miny(pusta_siatka, start, self.numer_min)
-
+        self.ustaw_miny(poczatkowa_komorka)
         for i, j in self.miny:
             pusta_siatka[i][j] = 'X'
 
-        siatka = self.pobierz_numery(pusta_siatka)
-        self.plansza = siatka
+        self.plansza = self.pobierz_dystanse_do_min(pusta_siatka)
 
-    # TODO: convert siatka -> self (if possible)
+    def stworz_plansze_dla_gracza(self):
+        self.plansza_widoczna_dla_gracza = [[' ' for i in range(self.rozmiar_siatki)] for i in
+                                            range(self.rozmiar_siatki)]
+
     def pokaz_plansze(self, siatka):
         poziomo = '   ' + (4 * self.rozmiar_siatki * '-') + '-'
 
-        # Wypisz litery u gory 'a b c d e f g h i'
+        # Wypisz litery u gory, np. 'a b c d e f g h i'
         najwyzsza_kolumna = '     '
 
         for i in ascii_lowercase[:self.rozmiar_siatki]:
@@ -56,44 +58,39 @@ class Siatka:
 
         print('')
 
-    # TODO: convert siatka -> self (if possible)
-    def pobierz_losowa_komorke(self, siatka):
-        rozmiar_siatki = len(siatka)
+    def pobierz_losowa_komorke(self):
+        a = random.randint(0, self.rozmiar_siatki - 1)
+        b = random.randint(0, self.rozmiar_siatki - 1)
 
-        a = random.randint(0, rozmiar_siatki - 1)
-        b = random.randint(0, rozmiar_siatki - 1)
+        return a, b
 
-        return (a, b)
-
-    def pobierz_sasiadow(self, siatka, numer_rzedu, numer_kolumny):
-        rozmiar_siatki = len(siatka)
+    def pobierz_sasiednie_komorki(self, numer_rzedu, numer_kolumny):
         sasiednie_komorki = []
 
         for i in range(-1, 2):
             for j in range(-1, 2):
                 if i == 0 and j == 0:
                     continue
-                elif -1 < (numer_rzedu + i) < rozmiar_siatki and -1 < (numer_kolumny + j) < rozmiar_siatki:
+                elif -1 < (numer_rzedu + i) < self.rozmiar_siatki and -1 < (numer_kolumny + j) < self.rozmiar_siatki:
                     sasiednie_komorki.append((numer_rzedu + i, numer_kolumny + j))
 
         return sasiednie_komorki
 
-    def ustaw_miny(self, siatka, start, liczba_min):
-        sasiednie_komorki = self.pobierz_sasiadow(siatka, *start)
+    def ustaw_miny(self, start):
+        sasiednie_komorki = self.pobierz_sasiednie_komorki(*start)
 
-        for i in range(liczba_min):
-            komorka = self.pobierz_losowa_komorke(siatka)
+        for i in range(self.numer_min):
+            komorka = self.pobierz_losowa_komorke()
             while komorka == start or komorka in self.miny or komorka in sasiednie_komorki:
-                komorka = self.pobierz_losowa_komorke(siatka)
+                komorka = self.pobierz_losowa_komorke()
             self.miny.append(komorka)
 
-    def pobierz_numery(self, siatka):
+    def pobierz_dystanse_do_min(self, siatka):
         for numer_rzedu, rzad in enumerate(siatka):
             for numer_kolumny, komorka in enumerate(rzad):
                 if komorka != 'X':
                     # Pobierz wartosci sasiednich komorek
-                    wartosci = [siatka[r][c] for r, c in self.pobierz_sasiadow(siatka,
-                                                                        numer_rzedu, numer_kolumny)]
+                    wartosci = [siatka[r][c] for r, c in self.pobierz_sasiednie_komorki(numer_rzedu, numer_kolumny)]
 
                     # Policz jak wiele jest min
                     siatka[numer_rzedu][numer_kolumny] = str(wartosci.count('X'))
@@ -101,35 +98,38 @@ class Siatka:
         return siatka
 
 
-    def pokaz_komorki(self, siatka, obecna_siatka, numer_rzedu, numer_kolumny):
+    def pokaz_komorki(self, siatka, plansza_widoczna_dla_gracza, numer_rzedu, numer_kolumny):
         # Wyjdz z funkcji jesli komorka jest juz pokazana
-        if obecna_siatka[numer_rzedu][numer_kolumny] != ' ':
+        if plansza_widoczna_dla_gracza[numer_rzedu][numer_kolumny] != ' ':
             return
 
         # Pokaz obecna komorke
-        obecna_siatka[numer_rzedu][numer_kolumny] = siatka[numer_rzedu][numer_kolumny]
+        plansza_widoczna_dla_gracza[numer_rzedu][numer_kolumny] = siatka[numer_rzedu][numer_kolumny]
 
         # Pobierz sasiadow jesli komorka jest pusta
         if siatka[numer_rzedu][numer_kolumny] == '0':
-            for r, c in self.pobierz_sasiadow(siatka, numer_rzedu, numer_kolumny):
+            for r, c in self.pobierz_sasiednie_komorki(numer_rzedu, numer_kolumny):
                 # Powtorz funkcje dla kazdego sasiada ktory nie ma flagi
-                if obecna_siatka[r][c] != 'F':
-                    self.pokaz_komorki(siatka, obecna_siatka, r, c)
+                if plansza_widoczna_dla_gracza[r][c] != 'F':
+                    self.pokaz_komorki(siatka, plansza_widoczna_dla_gracza, r, c)
 
 
 class Gra:
-    'Klasa ktora zarzadza gra.'
+    """Klasa ktora zarzadza gra."""
 
-    def parsuj_dane_wejsciowe(self, wejsciowy_string, rozmiar_siatki, pomocna_wiadomosc):
+    instrukcja = ("Wpisz kolumne a nastepnie numer rzedu (np. b1). "
+                       "Aby postawic lub usunac flage, dodaj 'f' do komorki (np. b1f)")
+
+    def parsuj_dane_wejsciowe(self, wejsciowy_string, rozmiar_siatki):
         komorka = ()
         flaga = False
-        wiadomosc = "Niepoprawna komorka. " + pomocna_wiadomosc
+        wiadomosc = "Niepoprawna komorka. " + self.instrukcja + '\n'
 
         wzorzec = r'([a-{}])([0-9]+)(f?)'.format(ascii_lowercase[rozmiar_siatki - 1])
         poprawny_string = re.match(wzorzec, wejsciowy_string)
 
         if wejsciowy_string == 'help':
-            wiadomosc = pomocna_wiadomosc
+            wiadomosc = self.instrukcja
 
         elif poprawny_string:
             numer_rzedu = int(poprawny_string.group(2)) - 1
@@ -145,25 +145,15 @@ class Gra:
 
     def zagraj(self):
         siatka = Siatka()
+        siatka.stworz_plansze_dla_gracza()
+        siatka.pokaz_plansze(siatka.plansza_widoczna_dla_gracza)
 
-        rozmiar_siatki = siatka.get_rozmiar_siatki()
-        numer_min = siatka.get_numer_min()
-
-        plansza_widoczna_dla_gracza = [[' ' for i in range(rozmiar_siatki)] for i in range(rozmiar_siatki)]
-
-        start_czasu = 0
-
-        pomocna_wiadomosc = ("Wpisz kolumne a nastepnie numer rzedu (np. b1). "
-                       "Aby postawic lub usunac flage, dodaj 'f' do komorki (np. b1f)")
-
-        siatka.pokaz_plansze(plansza_widoczna_dla_gracza)
-        print(pomocna_wiadomosc + " Wpisz 'help' aby pokazac ta wiadomosc ponownie.\n")
+        print(self.instrukcja + " Wpisz 'help' aby pokazac ta wiadomosc ponownie.\n")
+        start_czasu = time.time()
 
         while True:
-            pozostalo_min = numer_min - len(siatka.flagi)
-            komenda_popros = input('Podaj komorke ({} min zostalo): '.format(pozostalo_min))
-
-            wynik = self.parsuj_dane_wejsciowe(komenda_popros, rozmiar_siatki, pomocna_wiadomosc + '\n')
+            wpisane_dane = input('Podaj komorke: ')
+            wynik = self.parsuj_dane_wejsciowe(wpisane_dane, siatka.rozmiar_siatki)
 
             wiadomosc = wynik['wiadomosc']
             komorka = wynik['komorka']
@@ -171,22 +161,21 @@ class Gra:
             if komorka:
                 print('\n\n')
                 numer_rzedu, numer_kolumny = komorka
-                obecna_komorka = plansza_widoczna_dla_gracza[numer_rzedu][numer_kolumny]
+                obecna_komorka = siatka.plansza_widoczna_dla_gracza[numer_rzedu][numer_kolumny]
                 flaga = wynik['flaga']
 
                 if not siatka.plansza:
+                    # Utowrzy siatke z minami
                     siatka.stworz_plansze(komorka)
-                if not start_czasu:
-                    start_czasu = time.time()
 
                 if flaga:
                     # Dodaj flage jesli komorka jest pusta
                     if obecna_komorka == ' ':
-                        plansza_widoczna_dla_gracza[numer_rzedu][numer_kolumny] = 'F'
+                        siatka.plansza_widoczna_dla_gracza[numer_rzedu][numer_kolumny] = 'F'
                         siatka.flagi.append(komorka)
                     # Usun flage jesli flaga jest juz ustawiona
                     elif obecna_komorka == 'F':
-                        plansza_widoczna_dla_gracza[numer_rzedu][numer_kolumny] = ' '
+                        siatka.plansza_widoczna_dla_gracza[numer_rzedu][numer_kolumny] = ' '
                         siatka.flagi.remove(komorka)
                     else:
                         wiadomosc = 'Nie mozna wstawic tu flagi'
@@ -201,7 +190,7 @@ class Gra:
                     return
 
                 elif obecna_komorka == ' ':
-                    siatka.pokaz_komorki(siatka.plansza, plansza_widoczna_dla_gracza, numer_rzedu, numer_kolumny)
+                    siatka.pokaz_komorki(siatka.plansza, siatka.plansza_widoczna_dla_gracza, numer_rzedu, numer_kolumny)
 
                 else:
                     wiadomosc = "Ta komorka jest juz odkryta"
@@ -215,7 +204,7 @@ class Gra:
                     siatka.pokaz_plansze(siatka.plansza)
                     return
 
-            siatka.pokaz_plansze(plansza_widoczna_dla_gracza)
+            siatka.pokaz_plansze(siatka.plansza_widoczna_dla_gracza)
             print(wiadomosc)
 
 
